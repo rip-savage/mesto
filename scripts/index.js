2efc8538cc15546332fd0f32db5ebf6624feddc6
rip-savage <savage@quake2.com.ru>
2022-01-17 17:38:04 +0700

    перебрать массив

--- scripts/index.js
+++ scripts/index.js
@@ -49,3 +49,58 @@ function closeOnPopupAddCardOverlayClick(event) {
 popupOpenButtonAddCard.addEventListener('click', togglePopupAddCard);
 popupCloseButtonAddCard.addEventListener('click', togglePopupAddCard);
 popupAddCard.addEventListener('click', closeOnPopupAddCardOverlayClick);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+/*<li class="elements__item">
+      <img src="" class="elements__item-image" alt="">
+      <div class="elements__item-footer">
+        <h2 class="elements__item-title"></h2>
+        <button class="elements__item-button page__button" type="button"></button>
+      </div>
+    </li>*/
+
+initialCards.forEach(function(makeCard) {
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const list = document.querySelector('.elements');
+  const cardLi = cardTemplate.querySelector('.elements__item');
+  const cardFooter = cardTemplate.querySelector('.elements__item-footer');
+  const cardImage = cardTemplate.querySelector('.elements__item-image');
+  const cardTitle = cardTemplate.querySelector('.elements__item-title');
+  
+  cardTitle.textContent = makeCard.name;
+  cardImage.src = makeCard.link;
+
+  list.prepend(cardElement);
+  
+  console.log('cardData =>', makeCard);
+  console.log('cardImage =>', cardImage);
+  console.log('cardTitle =>', cardTitle);
+  console.log('cardLi =>', cardLi);
+});
